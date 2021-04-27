a08507c0d8df28a4daf2f20ab4dd29302641adbf
Matthijs De Smedt <matthijsds@gmail.com>
2021-04-27 15:47:34 -0700

    Comments and naming

--- src/bvhiter.rs
+++ src/bvhiter.rs
@@ -5,8 +5,8 @@ pub struct BVHIterator<'a> {
     bvh: &'a BVH,
     ray: bvh::ray::Ray,
     stack: [usize; 32],
-    current_node: usize,
-    current_stack: usize,
+    node_index: usize,
+    stack_size: usize,
     has_node: bool,
 }
 
@@ -15,36 +15,41 @@ impl<'a> BVHIterator<'a> {
         BVHIterator {
             bvh: bvh,
             ray: ray,
-            stack: [0; 32],
-            current_node: 0,
-            current_stack: 0,
-            has_node: true,
+            stack: [0; 32], // 4 billion items seems enough?
+            node_index: 0,
+            stack_size: 0,
+            has_node: true, // Whether or not we have a valid node (or leaf)
         }
     }
 
-    fn stack_empty(&self) -> bool {
-        return self.current_stack == 0;
+    /// Test if stack is empty.
+    fn is_stack_empty(&self) -> bool {
+        return self.stack_size == 0;
     }
 
+    /// Push node onto stack. Not guarded against overflow.
     fn stack_push(&mut self, node: usize) {
-        self.stack[self.current_stack] = node;
-        self.current_stack += 1;
+        self.stack[self.stack_size] = node;
+        self.stack_size += 1;
     }
 
+    /// Pop the stack and return the node. Not guarded against underflow.
     fn stack_pop(&mut self) -> usize {
-        self.current_stack -= 1;
-        return self.stack[self.current_stack];
+        self.stack_size -= 1;
+        return self.stack[self.stack_size];
     }
 
+    /// Attempt to move to the left child of the current node.
     fn move_left(&mut self) {
-        match self.bvh.nodes[self.current_node] {
+        match self.bvh.nodes[self.node_index] {
             BVHNode::Node {
                 child_l_index,
                 ref child_l_aabb,
                 ..
             } => {
                 if self.ray.intersects_aabb(child_l_aabb) {
-                    self.current_node = child_l_index;
+                    self.node_index = child_l_index;
+                    self.has_node = true;
                 } else {
                     self.has_node = false;
                 }
@@ -55,15 +60,17 @@ impl<'a> BVHIterator<'a> {
         }
     }
 
+    /// Attempt to move to the right child of the current node.
     fn move_right(&mut self) {
-        match self.bvh.nodes[self.current_node] {
+        match self.bvh.nodes[self.node_index] {
             BVHNode::Node {
                 child_r_index,
                 ref child_r_aabb,
                 ..
             } => {
                 if self.ray.intersects_aabb(child_r_aabb) {
-                    self.current_node = child_r_index;
+                    self.node_index = child_r_index;
+                    self.has_node = true;
                 } else {
                     self.has_node = false;
                 }
@@ -80,20 +87,25 @@ impl<'a> Iterator for BVHIterator<'a> {
 
     fn next(&mut self) -> Option<usize> {
         loop {
-            if self.stack_empty() && !self.has_node {
+            if self.is_stack_empty() && !self.has_node {
+                // Completed traversal.
                 break;
             }
             if self.has_node {
-                self.stack_push(self.current_node);
+                // If we have any node, save it and attempt to move to its left child.
+                self.stack_push(self.node_index);
                 self.move_left();
             } else {
-                self.current_node = self.stack_pop();
-                match self.bvh.nodes[self.current_node] {
+                // Go back up the stack and see if a node or leaf was pushed.
+                self.node_index = self.stack_pop();
+                match self.bvh.nodes[self.node_index] {
                     BVHNode::Node { .. } => {
-                        self.has_node = true;
+                        // If a node was pushed, now attempt to move to its right child.
                         self.move_right();
                     }
                     BVHNode::Leaf { shape_index, .. } => {
+                        // We previously pushed a leaf node. This is the "visit" of the in-order traverse.
+                        // Next time we call next we try to pop the stack again.
                         self.has_node = false;
                         return Some(shape_index);
                     }
